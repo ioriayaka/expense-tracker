21a3d54477a64f62685d775178b2d33a4365754b
ioriayaka <ioriayaka@hotmail.com>
2021-07-25 20:54:51 +0800

    test

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/express-tracker'
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
