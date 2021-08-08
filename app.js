05889991191cc7ceb12e4053812995625d3f586f
ioriayaka <ioriayaka@hotmail.com>
2021-08-08 14:43:14 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ const PORT = process.env.PORT || 3000
 require('./config/mongoose')
 app.engine('hbs', exphbs({
   defaultLayout: 'main', extname: '.hbs',
-    helpers: {
-      toDate: function (date) {
-        return new Date(date.getTime() - (date.getTimezoneOffset() * 60 * 1000))
-          .toISOString()
-          .split("T")[0]
-      }
+  helpers: {
+    toDate: function (date) {
+      return new Date(date.getTime() - (date.getTimezoneOffset() * 60 * 1000))
+        .toISOString()
+        .split("T")[0]
     }
- }))
+  }
+}))
 app.set('view engine', 'hbs')
 app.use(session({
   secret: 'ThisIsMySecret',
@@ -30,6 +30,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app)
+app.use((req, res, next) => {
+  console.log(req.user)
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 app.use(routes)
 
 app.listen(PORT, () => {
